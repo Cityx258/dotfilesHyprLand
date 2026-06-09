a9f6fe7f8c1423a391247c6f07f67376a866e3f1
Cityx258 <112200759+Cityx258@users.noreply.github.com>
2026-06-09 13:12:58 -0400

    feat: made wallpaper picker way better

--- rofi/WallpaperSwapper.sh
+++ rofi/WallpaperSwapper.sh
@@ -2,7 +2,13 @@
 
 WALLPAPER_DIR="$HOME/Wallpapers"
 
-chosen=$(ls "$WALLPAPER_DIR" | rofi -show drun -dmenu -p "Wallpaper")
+chosen=$(
+	for img in "$WALLPAPER_DIR"/*; do
+		[ -f "$img" ] || continue
+		name=$(basename "$img")
+		printf '%s\x00icon\x1f%s\n' "$name" "$img"
+	done | rofi -dmenu -p "Wallpaper" -show-icons -theme ~/.config/rofi/wallpaper.rasi
+)
 
 if [ -n "$chosen" ]; then
 	awww img --transition-type wipe --transition-duration 1 "$WALLPAPER_DIR/$chosen"
